a96c9aae45a7cbd85e3085eedb08b7eb749d3348
Gardashaliyeva <94057319+Gardashaliyeva@users.noreply.github.com>
2023-09-28 18:32:10 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.sonarqube") version "4.3.1.3277"
 }
 
 group = "org.example"
@@ -12,6 +13,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "bhos-qa_l3-sonarcloud-Gardashaliyeva")
+    property("sonar.organization", "bhos-qa")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
 
 tasks.test {
