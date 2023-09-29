d87a00268b3d133ff5fb6525b94c685a97ed67f3
Ilaha Gardashaliyeva <ilahagardashaliyeva@gmail.com>
2023-09-29 10:37:03 +0400

    Modification

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,38 @@
 plugins {
-    id("java")
+    id ("java")
+    id ("jacoco")
+    id ("org.sonarqube") version "4.3.1.3277"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+sonar {
+    properties {
+        property ("sonar.projectKey", "bhos-qa_l3-sonarcloud-Gardashaliyeva")
+        property ("sonar.organization", "bhos-qa")
+        property ("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+group = 'org.example'
+version = '1.0-SNAPSHOT'
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
+jacocoTestReport {
+    reports {
+        xml.required = true
+        html.required = true
+    }
 }
 
-tasks.test {
+dependencies {
+    testImplementation platform('org.junit:junit-bom:5.9.1')
+    testImplementation 'org.junit.jupiter:junit-jupiter'
+}
+
+
+test {
     useJUnitPlatform()
 }
